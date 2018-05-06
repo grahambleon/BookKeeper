ba020bd788ccb156e5ad32f887d86ba723654492
Dan Pickett <dan.pickett@launchacademy.com>
2018-05-06 19:27:22 -0400

    fix jest configuration

--- spec/javascript/setupTests.js
+++ spec/javascript/setupTests.js
@@ -1,5 +1,5 @@
 const Enzyme = require('enzyme');
 
-const EnzymeAdapter = require('enzyme-adapter-react-15');
+const EnzymeAdapter = require('enzyme-adapter-react-15.4');
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });

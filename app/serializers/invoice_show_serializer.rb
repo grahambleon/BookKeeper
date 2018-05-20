7e372826e3e23a41f275734922d17dbcffd68ab9
Graham Gilpatric <grahamwithag@gmail.com>
2018-05-20 19:08:22 -0400

    Added account name to invoice

--- app/serializers/invoice_show_serializer.rb
+++ app/serializers/invoice_show_serializer.rb
@@ -2,4 +2,5 @@ class InvoiceShowSerializer < ActiveModel::Serializer
   attributes :id, :invoice_number, :amount, :date_received, :account_id, :invoice_image
 
   has_many :purchases, serializer: PurchasesSerializer
+  has_one :account, serializer: AccountIndexSerializer
 end

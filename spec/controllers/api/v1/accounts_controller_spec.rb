3614d5158fb881312020df4f8baf1c751a25ab02
Graham Gilpatric <grahamwithag@gmail.com>
2018-06-11 23:55:10 -0400

    refactoring

--- spec/controllers/api/v1/accounts_controller_spec.rb
+++ spec/controllers/api/v1/accounts_controller_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe Api::V1::AccountsController, type: :controller do
   let!(:account) { Account.create!(company_name: "Russo's", user: user) }
   let!(:invoice) { Invoice.create!(invoice_number: "1337", amount: 200.00, date_received: "2/4/2017", account: account, user: user) }
   let!(:purchase) { Purchase.create!(product_id: "0293874", product_name: "Taters", quantity: "30 lbs",  unit_price: 3.00, total_price: 90.00, account: account, user: user, invoice: invoice) }
-
+  before(:each) do
+    sign_in user
+  end
   describe "GET#index" do
     it "should return a list of all accounts and their basic info" do
-
-      sign_in user
 
       get :index
       returned_json = JSON.parse(response.body)
@@ -25,8 +25,6 @@ RSpec.describe Api::V1::AccountsController, type: :controller do
   describe "GET#show" do
     it "should return a specific account and their basic info and corresponding invoices and purchases" do
 
-      sign_in user
-
       get :show, params: { id: account.id }
       returned_json = JSON.parse(response.body)
 
@@ -41,8 +39,6 @@ RSpec.describe Api::V1::AccountsController, type: :controller do
 
   describe "GET#graph" do
     it "should return a specific account and its invoices" do
-
-      sign_in user
 
       get :graph, params: { id: account.id }
       returned_json = JSON.parse(response.body)
@@ -63,16 +59,13 @@ RSpec.describe Api::V1::AccountsController, type: :controller do
 
     it "creates a new account" do
 
-      sign_in user
-
       prev_count = Account.count
       post(:create, format: JSON, params: @post_json)
       expect(Account.count).to eq(prev_count + 1)
     end
 
     it "Returns a JSON with the new account" do
-      sign_in user
-
+      
       post(:create, format: JSON, params: @post_json)
       returned_json = JSON.parse(response.body)
       expect(response.status).to eq 200

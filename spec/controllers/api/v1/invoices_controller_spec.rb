bef7b950b9a90f91d467910907f5ffc417107005
Graham Gilpatric <grahamwithag@gmail.com>
2018-06-11 23:57:51 -0400

    more refactoring

--- spec/controllers/api/v1/invoices_controller_spec.rb
+++ spec/controllers/api/v1/invoices_controller_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::InvoicesController, type: :controller do
+
   let!(:user) { User.create!(email: "me@aol.com", business_name: "ehhh", password: "123456", address:"123 fake street") }
   let!(:account) { Account.create!(company_name: "Russo's", user: user) }
   let!(:invoice) { Invoice.create!(invoice_number: "1337", amount: 200.00, date_received: "2/4/2017", account: account, user: user) }
   let!(:purchase) { Purchase.create!(product_id: "0293874", product_name: "Taters", quantity: "30 lbs",  unit_price: 3.00, total_price: 90.00, account: account, user: user, invoice: invoice) }
+  before(:each) do
+    sign_in user
+  end
 
   describe "GET#index" do
     it "should return a list of all invoices and their basic info" do
-
-      sign_in user
 
       get :index
       returned_json = JSON.parse(response.body)
@@ -25,8 +27,6 @@ RSpec.describe Api::V1::InvoicesController, type: :controller do
   describe "GET#show" do
     it "should return a specific invoice" do
 
-      sign_in user
-
       get :show, params: { id: invoice.id }
       returned_json = JSON.parse(response.body)
 
@@ -37,7 +37,7 @@ RSpec.describe Api::V1::InvoicesController, type: :controller do
     end
 
     it "should return a specific invoice's account ID" do
-      sign_in user
+
 
       get :show, params: { id: invoice.id }
       returned_json = JSON.parse(response.body)
@@ -46,7 +46,7 @@ RSpec.describe Api::V1::InvoicesController, type: :controller do
     end
 
     it "should return a specific invoice's purchases" do
-      sign_in user
+
 
       get :show, params: { id: invoice.id }
       returned_json = JSON.parse(response.body)
@@ -56,7 +56,7 @@ RSpec.describe Api::V1::InvoicesController, type: :controller do
     end
 
     it "should create a new invoice" do
-      sign_in user
+
       post_json = { invoice_number: "1337", amount: "13.37", date_received: "05-01-2018", account_id: account }
 
       prev_count = Invoice.count
@@ -65,7 +65,7 @@ RSpec.describe Api::V1::InvoicesController, type: :controller do
     end
 
     it "should create a new invoice" do
-      sign_in user
+
       post_json = { invoice_number: "1337", amount: "13.37", date_received: "05-01-2018", account_id: account, invoice_image: '<File:/var/folders/pp/bdccvc4x0_16_llbv4dvv7f80000gn/T/RackMultipart20180605-85518-1py7jyo.jpg>'}
 
       post(:create, format: JSON, params: post_json)
@@ -78,7 +78,7 @@ RSpec.describe Api::V1::InvoicesController, type: :controller do
     end
 
     it "should create the purchases associated with the invoice" do
-      sign_in user
+
       post_json = {
         invoice_number: "1337",
         amount: "13.37",

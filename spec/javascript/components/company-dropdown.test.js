6fbb52178e97b4d2ede40ad83a3dba9d867bcd1c
Graham Gilpatric <grahamwithag@gmail.com>
2018-06-16 23:39:47 -0400

    test function call

--- spec/javascript/components/company-dropdown.test.js
+++ spec/javascript/components/company-dropdown.test.js
@@ -3,16 +3,24 @@ import { shallow, mount, render } from 'enzyme';
 import CompanyDropdown from '../../../app/javascript/components/company-dropdown';
 import fakeAccountIndex from '../fixtures/fakeAccountIndex.json'
 
+const fakeHandleChange = jest.fn();
+
 describe('<CompanyDropdown />', () => {
 
   const wrapper = mount(
     <CompanyDropdown
       companyList={fakeAccountIndex}
       label='Account'
+      handleChange={fakeHandleChange}
     />
   )
 
   it('renders properly', () => {
     expect(wrapper.length).toBe(1);
   })
+
+  it("when simulating a change, select should update its value", () => {
+    wrapper.find('select').simulate('change',{target: { value : '2'}});
+    expect(fakeHandleChange).toHaveBeenCalled();
+  });
 })
